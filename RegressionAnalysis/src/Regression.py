b0c734b172e5cac7c16e9f23eb3407b860ed5ed8
narci nar <jwch11@gmail.com>
2017-10-11 06:37:56 +0900

    Function

--- RegressionAnalysis/src/Regression.py
+++ RegressionAnalysis/src/Regression.py
@@ -3,21 +3,24 @@ import numpy as np
 
 num_points = 50
 degree = 6
-hyper = 1000
+hyper = 0
 learning_rate = 0.001
 
-x_data = [[0 for col in range(num_points)] for row in range(degree)]
-y_data = []
-prev_W = [0.5,0.5,1,1,1,0]
 
-for i in range(num_points):
-    prev_x = np.random.normal(0, 1.0)
+def factory():
+    x_data = [[0 for col in range(num_points)] for row in range(degree)]
+    y_data = []
+    prev_W = [0.5,0.5,1,1,1,0]
+
+    for i in range(num_points):
+        x = np.random.normal(0, 1.0)
          
-    for j in range(degree):
-        x_data[j][i] = pow(prev_x,j+1)
+        for j in range(degree):
+            x_data[j][i] = pow(x,j+1)
 
-y_data = np.dot(prev_W, x_data)
+    y_data = np.dot(prev_W, x_data)
 
+    return x_data[0], y_data
 
 #######################
 
@@ -25,68 +28,84 @@ y_data = np.dot(prev_W, x_data)
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
+def regression(argx_data, argy_data):
+    y_data = argy_data
 
+    degree_level = 10
 
-for degree_hat in range(1, degree):
-    weights = []
-    for i in range(degree_hat):
-        weights.append(tf.Variable(tf.ones([1])))
-    weights = tf.transpose(weights)
+    plt.plot(argx_data, argy_data, 'ro')
+    
+    for degree_hat in range(1, degree_level):
+        #x init
+        x_data = [[0 for col in range(num_points)] for row in range(degree_hat)]
+        for i in range(num_points):
+            x = argx_data[i]
+    
+            for j in range(degree_hat):
+                x_data[j][i] = pow(x,j+1)
 
-    y_hat = tf.matmul(weights, x_data[0:degree_hat])
-
-
-
-    cost = tf.square(y_hat - y_data)
-
-    regularization = tf.reduce_mean(tf.square(weights)*hyper)
-
-
-    loss = tf.reduce_mean(cost+regularization) #+ lamda * tf.reduce_sum(tf.square(ws))
-
-
-
-
-
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-    train = optimizer.minimize(loss)
-
-
-    init = tf.global_variables_initializer()
-    sess = tf.Session()
-    sess.run(init)
+        #w init
+        weights = []
+        for i in range(degree_hat):
+            weights.append(tf.Variable(tf.ones([1])))
+        weights = tf.transpose(weights)
 
 
 
-
-    for step in range(500):
-        sess.run(train) 
-    # 산포도 그리기
+        
 
 
-    plt.plot(x_data[0], y_data, 'ro')
-    # 직선 그리기
+        y_hat = tf.matmul(weights, x_data[0:degree_hat])
 
-    draw_x_data = [[0 for col in range(100)] for row in range(degree_hat)]
-    draw_x_data[0] = np.linspace(-10,10,100)
+        cost = tf.square(y_hat - y_data)
 
+        regularization = tf.reduce_mean(tf.square(weights) * hyper)
 
-    for i in range(100):
-        for j in range(degree_hat-1):
-            draw_x_data[j+1][i] = pow(draw_x_data[0][i],j+2)
-
-    draw_y_data = np.dot(sess.run(weights),draw_x_data)
+        loss = tf.reduce_mean(cost + regularization)
 
 
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+        train = optimizer.minimize(loss)
 
-    plt.plot(draw_x_data[0], np.transpose(draw_y_data))
-    print(sess.run(weights))
+
+        init = tf.global_variables_initializer()
+        sess = tf.Session()
+        sess.run(init)
 
 
-# x, y 축 레이블링을 하고 각 축의 최대, 최소값 범위를 지정합니다.
+        for step in range(500):
+            sess.run(train) 
 
-plt.xlim(-2,2)
-plt.ylim(-30, 120)
-plt.xlabel('x')
-plt.ylabel('y')
-plt.show()
+
+
+
+
+
+
+        
+        # draw
+
+        draw_x_data = [[0 for col in range(100)] for row in range(degree_hat)]
+        draw_x_data[0] = np.linspace(-10,10,100)
+
+
+        for i in range(100):
+            for j in range(degree_hat-1):
+                draw_x_data[j+1][i] = pow(draw_x_data[0][i],j+2)
+
+        draw_y_data = np.dot(sess.run(weights),draw_x_data)
+
+
+
+        plt.plot(draw_x_data[0], np.transpose(draw_y_data))
+        print(sess.run(weights))
+
+    plt.xlim(-2,2)
+    plt.ylim(-30, 120)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+
+
+argx_data, argy_data = factory()
+regression(argx_data, argy_data)
