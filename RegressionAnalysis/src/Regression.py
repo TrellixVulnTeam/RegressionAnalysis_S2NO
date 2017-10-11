293de63f300194c1750e55214bc7f6db7e0f0d62
narci nar <jwch11@gmail.com>
2017-10-12 05:04:32 +0900

    Implement improved guI Regularization add (ridge/ lasso/ elastic net)

--- RegressionAnalysis/src/Regression.py
+++ RegressionAnalysis/src/Regression.py
@@ -4,8 +4,8 @@ import tensorflow as tf
 
 def polynomial_regression(y_data, x_data, order_level, option):
     # Extend data to enter polynomial variable.
-    poly_x_data = [[pow(x, order) for x in x_data] for order in range(0, order_level)]
-    weights = [tf.Variable(tf.ones([1])) for order in range(0, order_level)]
+    poly_x_data = [[pow(x, order) for x in x_data] for order in range(0, order_level+1)]
+    weights = [tf.Variable(tf.ones([1])) for order in range(0, order_level+1)]
 
     return regression(y_data, poly_x_data, tf.transpose(weights), option=option)
 
@@ -16,7 +16,12 @@ def regression(y_data, x_data, weights, option):
 
     cost = tf.square(y_hat - y_data)
 
-    regularization = tf.reduce_mean(tf.square(weights) * option["hyper"])
+    if option["regularization"] is "ridge":
+        regularization = tf.reduce_mean(tf.square(weights) * option["hyper"])
+    elif option["regularization"] is "lasso":
+        regularization = tf.reduce_mean(tf.abs(weights) * option["hyper"])
+    elif option["regularization"] is "elastic_net":
+        regularization = tf.reduce_mean(tf.square(weights) * option["hyper"] + tf.abs(weights) * option["hyper2"])
 
     loss = tf.reduce_mean(cost + regularization)
 
@@ -33,5 +38,4 @@ def regression(y_data, x_data, weights, option):
     for step in range(option["steps"]):
         sess.run(train) 
 
-
-    return sess.run(weights)
+    return sess.run(weights), np.mean(sess.run(cost))
--- RegressionAnalysis/src/gui.py
+++ RegressionAnalysis/src/gui.py
@@ -5,12 +5,15 @@ from Regression import regression, polynomial_regression
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 
 class XMainWindow(QMainWindow, Ui_MainWindow):
+    order_level = 10+1
     x_data = []
     y_data = []
     option = {}
+    result = {} # [weights,error_rate]
 
     def __init__(self):
         QMainWindow.__init__(self)
@@ -24,21 +27,37 @@ class XMainWindow(QMainWindow, Ui_MainWindow):
 
         self.axesInit()
 
+        # regression option
+        self.option = {"learning_rate": 0.01,
+                       "steps": 500,
+                       "regularization": "ridge",
+                       "hyper": 0,
+                       "hyper2": 0}
+
+        self.result = dict([i,[[],0]] for i in range(0,self.order_level))
+
         # event handler
         self.dial.valueChanged.connect(self.dialChanged_Event)
+        self.dial_2.valueChanged.connect(self.dial2Changed_Event)
         self.clearButton.clicked.connect(self.clearButtonClicked_Event)
         self.excuteButton.clicked.connect(self.excuteButtonClick_Event)
         self.actionGoto_gitHub.triggered.connect(lambda: webbrowser.open("https://github.com/NarciSource/RegressionAnalysis"))
         self.actionAbout.triggered.connect(self.actionAbout_Event)
+        self.learningrateEdit.textChanged.connect(self.lEditChanged_Event)
+        self.stepsEdit.textChanged.connect(self.stepsEditChanged_Event)
+        self.comboBox.currentIndexChanged.connect(self.comboBoxChanged_Event)
         self.actionExit.triggered.connect(self.close)
+        self.radioButton.clicked.connect(self.radioButtonClicked_Event)
+        self.radioButton_2.clicked.connect(self.radioButtonClicked_Event)
+        self.radioButton_3.clicked.connect(self.radioButtonClicked_Event)
         
-        # regression option
-        self.option = {"hyper": 0,
-                       "learning_rate": 0.01,
-                       "steps": 500}
-    
+        for i in range(2, self.order_level):
+            self.comboBox.addItem(str(i)+" polynomial")
+
+
     def axesInit(self):
         self.axes_subplot.clear()
+        self.axes_subplot.set_title("Regression analysis Plot")
         self.axes_subplot.set_xlim(-2,2)
         self.axes_subplot.set_ylim(-30, 120)
         self.axes_subplot.set_xlabel('x')
@@ -48,32 +67,35 @@ class XMainWindow(QMainWindow, Ui_MainWindow):
 
     ## process method
     def core(self):
-        order_level = 10
-
-        for order in range(2, order_level):
-            esst_weights = polynomial_regression(self.y_data, self.x_data, order, option=self.option)
-            self.draw_line(order, esst_weights)
-
-        self.draw()
-
+        for order in range(1, self.order_level):
+            self.result[order] = polynomial_regression(self.y_data, self.x_data, order, option=self.option)
+        
 
     def draw(self):
         self.canvas.draw()
 
 
-    def draw_line(self, order, weights):
+    def draw_line(self, num):
         smoth = 100
         line_xlim, line_ylim = (-10,10)
 
         line_data = np.linspace(line_xlim,line_ylim,smoth)
+        weight = self.result[num][0]
 
-        draw_x_data = [[pow(x, order) for x in line_data] for order in range(0, order)]
-        draw_y_data = np.dot(weights, draw_x_data)
+        if num is 0:
+            for i in range(1,self.order_level):
+                self.draw_line(i)
+        elif len(weight) is not 0:
+            draw_x_data = [[pow(x, order) for x in line_data] for order in range(0, num+1)]
+            draw_y_data = np.dot(weight, draw_x_data)
 
-        self.axes_subplot.plot(line_data, np.transpose(draw_y_data),label = str(order-1)+" order")
-        print(str(order)+": "+str(weights))
-        
+            print(str(num)+": "+str(weight))
+            self.axes_subplot.plot(line_data, np.transpose(draw_y_data),label = str(num)+" order")
+        else:
+            None
 
+    def draw_point(self):
+        self.axes_subplot.plot(self.x_data, self.y_data, 'ro')
 
     def data_factory(self, point_x, point_y):
         margin_bottom, margin_left = (54,60)
@@ -89,13 +111,17 @@ class XMainWindow(QMainWindow, Ui_MainWindow):
         self.x_data.append(x)
         self.y_data.append(y)
 
-        self.axes_subplot.plot(self.x_data, self.y_data, 'ro')
+        self.draw_point()
         self.draw()
 
 
     ## event handler
     def excuteButtonClick_Event(self):
+        self.axesInit()
         self.core()
+        self.draw_point()
+        self.draw_line(0) # 0 is all
+        self.draw()        
 
     def clearButtonClicked_Event(self):
         self.x_data = []
@@ -103,34 +129,63 @@ class XMainWindow(QMainWindow, Ui_MainWindow):
         self.axesInit()
         self.draw()
 
-    def actionAbout_Event(self):
-        msgbox = QMessageBox(self)
-        msgbox.information(self,"About","Chung Ang Univ\nName : Jeong Won Cheol\nStudent Code : 20113560")
-
     def dialChanged_Event(self):
+        self.dialLabel.setText(str(self.dial.value()))
         self.option["hyper"] = self.dial.value()
-        regression(self.x_data, self.y_data, order_hat=4, paper=self.axes_subplot, option=self.option)
-        self.draw()
 
-    def mousePosition_onCanvas_Event(self, event):
-        self.data_factory(event.x(), event.y())        
+    def dial2Changed_Event(self):
+        self.dialLabel2.setText(str(self.dial_2.value()))
+        self.option["hyper2"] = self.dial_2.value()
+
+    def comboBoxChanged_Event(self, item):
+        print(self.result[item][1])
+        if not math.isnan(self.result[item][1]):
+            self.errorrateLCD.display(self.result[item][1])
+        else:
+            self.errorrateLCD.display("NAN")
+        self.axesInit()
+        self.draw_point()
+        self.draw_line(item)
+        self.draw()
 
     def mouseWheel_onCanvas_Event(self, event):
         wheelup,wheeldown = (120,-120)
+        xlim_begin,xlim_end = self.axes_subplot.get_xlim()
+        ylim_begin,ylim_end = self.axes_subplot.get_ylim()
         rate = 0.1
         if event.angleDelta().y() == wheelup:
-            xlim_begin,xlim_end = self.axes_subplot.get_xlim()
-            ylim_begin,ylim_end = self.axes_subplot.get_ylim()
             self.axes_subplot.set_xlim(xlim_begin*(1-rate),xlim_end*(1-rate))
             self.axes_subplot.set_ylim(ylim_begin*(1-rate),ylim_end*(1-rate))
             self.draw()
         elif event.angleDelta().y() == wheeldown:
-            xlim_begin,xlim_end = self.axes_subplot.get_xlim()
-            ylim_begin,ylim_end = self.axes_subplot.get_ylim()
             self.axes_subplot.set_xlim(xlim_begin*(1+rate),xlim_end*(1+rate))
             self.axes_subplot.set_ylim(ylim_begin*(1+rate),ylim_end*(1+rate))
             self.draw()
 
+    def mousePosition_onCanvas_Event(self, event):
+        self.data_factory(event.x(), event.y())  
+
+    def actionAbout_Event(self):
+        msgbox = QMessageBox(self)
+        msgbox.information(self,"About","Chung Ang Univ\nName : Jeong Won Cheol\nStudent Code : 20113560")
+
+    def lEditChanged_Event(self):
+        self.option["learning_rate"] = float(self.learningrateEdit.text())
+
+    def stepsEditChanged_Event(self):
+        self.option["steps"] = int(self.stepsEdit.text())
+
+    def radioButtonClicked_Event(self):
+        self.dial_2.setEnabled(False)
+        self.dial_2.setNotchesVisible(False)
+        if self.radioButton.isChecked():
+            self.option["regularization"] = "ridge"
+        elif self.radioButton_2.isChecked():
+            self.option["regularization"] = "lasso"
+        elif self.radioButton_3.isChecked():
+            self.option["regularization"] = "elastic_net"
+            self.dial_2.setEnabled(True)
+            self.dial_2.setNotchesVisible(True)
 
 class MyFigureCanvas(FigureCanvas):
     def __init__(self,figure,object):
